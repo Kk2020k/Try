acca82712ef929f6d9a1a302be22c9e165292e32
Kk2020k <108136211+Kk2020k@users.noreply.github.com>
2026-08-30 22:38:32 +0530

    Refactor StreamBot name to use Var.name

--- Adarsh/bot/__init__.py
+++ Adarsh/bot/__init__.py
@@ -1,14 +1,16 @@
 from pyrogram import Client
 from convopyro import Conversation
-from ..vars import Var  # Make sure your Var.py is in the parent directory
+
+from ..vars import Var
+
 
 StreamBot = Client(
-    name='Web Streamer',
+    name=Var.name,
     api_id=Var.API_ID,
     api_hash=Var.API_HASH,
     bot_token=Var.BOT_TOKEN,
     sleep_threshold=Var.SLEEP_THRESHOLD,
-    workers=Var.WORKERS
+    workers=Var.WORKERS,
 )
 
 Conversation(StreamBot)
